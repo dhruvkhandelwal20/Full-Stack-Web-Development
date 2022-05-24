7ee0d4b04c03a09d8c0eba0a8b98dc896f482930
Dhruv Khandelwal <dhruvkhandelwal39@gmail.com>
2022-05-24 22:48:47 +0530

    Developing the First API from scratch!

--- NODE JS/FIRST API/server.js
+++ NODE JS/FIRST API/server.js
@@ -1,14 +1,52 @@
 const express = require("express");
-const app = express();
+const server = express();
+const bodyParser = require("body-parser");
 
-app.get("/", function (req, res) {
-  res.send("This is the First API!");
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: false }));
+
+var ingredients = [
+  {
+    id: "O76niS",
+    name: "Onions",
+  },
+  {
+    id: "45gfh",
+    name: "Eggs",
+  },
+  {
+    id: "dk412",
+    name: "Boil Eggs",
+  },
+  {
+    id: "90sjk",
+    name: "fish",
+  },
+];
+
+// server.get('/', function(req, res){
+//   res.send("This is the First API!");
+// });
+
+// server.get('/funions', function(req, res){
+//   res.send("Yoo! Here are my Funions!");
+// });
+
+server.get("/", function (req, res) {
+  res.send(ingredients);
 });
 
-app.get("/funions", function (req, res) {
-  res.send("Yoo! Here are the funions foo!");
+server.post("/", function (req, res) {
+  var ingredient = req.body;
+  if (!ingredient || ingredient.name === "") {
+    res.status(500).send({ error: "Ingredient must have a Name!" });
+  } else {
+    ingredients.push(ingredient);
+    res.status(200).send(ingredient);
+  }
 });
 
-app.listen(3000, function () {
-  console.log("Server is running at Port: 3000");
+const portNumber = 3000;
+server.listen(portNumber, function () {
+  console.log(`Server is running at Port: ${portNumber}`);
 });
